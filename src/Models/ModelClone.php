f73e7f0399771316cd18451ee50684adafdab35c
memarius <me-marius@web.de>
2025-03-14 17:05:47 +0100

    add clone exemptable relationship

--- src/Models/ModelClone.php
+++ src/Models/ModelClone.php
@@ -2,8 +2,11 @@
 
 namespace Bkwld\Cloner\Models;
 
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Relations\MorphedByMany;
 
 class ModelClone extends Model
 {
@@ -11,9 +14,28 @@ class ModelClone extends Model
 
     protected $guarded = [];
 
+    protected $casts = ['additional_attributes' => AsArrayObject::class];
+
+    /*
+    *   additional_attributes.clone_exempt_classes: array of strings ['App\Models\User', ...]
+    * 
+    */
+
 
     public function modelCloneProgresses()
     {
         return $this->hasMany(ModelCloneProgress::class);
+    }
+
+
+    public function cloneExempts(string $modelClass): MorphToMany
+    {
+        return $this->morphedByMany($modelClass, 'exemptable', 'model_clone_exemptable', 'model_clone_id');
+    }
+
+
+    public function allCloneExempts(): MorphToMany
+    {
+        return $this->morphedByMany('*', 'exemptable', 'model_clone_exemptable', 'model_clone_id');
     }
 }
